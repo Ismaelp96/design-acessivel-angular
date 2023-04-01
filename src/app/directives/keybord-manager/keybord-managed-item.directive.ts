a1dd62aa12bce0aabc5d547e8b774659b36d4226
Ismael Oliveira <ismael6991@live.com>
2023-04-01 12:43:46 -0300

    Finalizando a lógica para ter um button mais semelhante a um radio button

--- src/app/directives/keybord-manager/keybord-managed-item.directive.ts
+++ src/app/directives/keybord-manager/keybord-managed-item.directive.ts
@@ -1,13 +1,15 @@
-import { Directive, ElementRef } from '@angular/core';
+import { Directive, ElementRef, EventEmitter, Output } from '@angular/core';
 
 @Directive({
   selector: '[appKeyMItem]',
 })
 export class KeybordManagedItemDirective {
+  @Output() focused = new EventEmitter<void>();
   constructor(private elementRef: ElementRef<HTMLElement>) {}
 
   focus(): void {
     this.elementRef.nativeElement.focus();
+    this.focused.emit();
   }
 
   isFocused(): boolean {
